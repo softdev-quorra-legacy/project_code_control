eb3761141584ed3d7113e29307b603c9541a4e70
shch8713 <47016159+shch8713@users.noreply.github.com>
2019-03-11 22:19:48 -0600

    Update login.dart

--- login.dart
+++ login.dart
@@ -1,33 +1,35 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
 
 class Login extends StatefulWidget {
   @override
   _LoginState createState() => _LoginState();
+
 }
 
 class _LoginState extends State<Login> {
-  // TODO: Add text editing controllers (101)
+  bool passwordVisible; //declare passwordVisible
+
+  // ignore: must_call_super
+  void initState() {
+    passwordVisible = false;
+  }
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      body: Container(
         child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 40.0),
+          padding: EdgeInsets.symmetric(horizontal: 30.0),
           children: <Widget>[
             SizedBox(height: 60.0),
             Column(
               children: <Widget>[
-                Image.asset('assets/logoname2.png'),
+                Image.asset('assets/logoname2.png', height: 80.00),
                 SizedBox(height: 80.0),
               ],
             ),
-            SizedBox(height: 20.0),
-            // TODO: Wrap Username with AccentColorOverride (103)
-            // TODO: Remove filled: true values (103)
-            // TODO: Wrap Password with AccentColorOverride (103)
+            SizedBox(height: 30.0),
             // Add TextField widgets
             // [Name]
             TextField(
@@ -40,51 +42,50 @@ class _LoginState extends State<Login> {
               // spacer
             SizedBox(height: 0),
               // [Password]
-            TextField(
+
+            TextFormField(
+              keyboardType: TextInputType.text,
               controller: _passwordController,
+              obscureText: passwordVisible,
               decoration: InputDecoration(
                 //filled: true,
                 labelText: 'Password',
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    // Based on passwordVisible state choose the icon
+                    passwordVisible
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                    color: Theme.of(context).primaryColorDark,
+                  ),
+                  onPressed: () {
+                    // Update the state i.e. toogle the state of passwordVisible variable
+                    setState(() {
+                       passwordVisible
+                          ? passwordVisible = false
+                          : passwordVisible = true;
+                    });
+                  },
+                ),
               ),
-              obscureText: true,
             ),
-            // TODO: Add button bar (101)
-//            Stack(
-//              // TODO: Add a beveled rectangular border to CANCEL (103)
-//              children: <Widget>[
-//                // TODO: Add buttons (101)
-//                FlatButton(
-//                  child: Align(alignment: Alignment.centerRight, child: Text("Forgot Password?",
-//                  style: TextStyle(
-//                    decoration: TextDecoration.underline,
-//                  ),
-//                ),
-//                  ),
-//                  onPressed: () {
-//                  },
-//                ),
-//              ],
-//            ),
             ButtonBar(
-              // TODO: Add a beveled rectangular border to CANCEL (103)
               children: <Widget>[
-                // TODO: Add buttons (101)
                 FlatButton(
                   child: Align(alignment: Alignment.centerLeft, child: Text("Forgot Password?",
                     style: TextStyle(
-                      fontSize: 9.50, //The max size can fit in the button bar
+                      fontSize: 12.00, //The max size can fit in the button bar
                       decoration: TextDecoration.underline,
                     ),
-                  ),
+                    ),
                   ),
                   onPressed: () {
-
+                    Navigator.of(context).pushNamed('/forgot_password');
                   },
                 ),
                 ButtonTheme(
                   minWidth: 4.00,
                   height: 35.00,
-
                   child: RaisedButton(
                     child: const Text('Sign Up'),
                     textColor: Colors.orangeAccent,
@@ -93,11 +94,10 @@ class _LoginState extends State<Login> {
                     splashColor: Color(0xFF424242),//it revealed white only when it's in a pressed state
                     onPressed: () {
                       // TODO: Show the next page (101)
-                      Navigator.of(context).pop('/home');
+                      Navigator.of(context).pushNamed('/form');
                     },
                   ),
                 ),
-
                 ButtonTheme(
                   minWidth: 4.00,
                   height: 35.00,
@@ -108,8 +108,9 @@ class _LoginState extends State<Login> {
                     elevation: 4.0, //shadow of button
                     splashColor: Color(0xFF424242),//it revealed white only when it's in a pressed state
                     onPressed: () {
-                      // TODO: Show the next page (101)
-                      Navigator.of(context).pop('/home');
+                      //if (_usernameController.TextEditingController == 'user' && _passwordController == 'password')
+                      Navigator.of(context).pushNamed('/home'); //Direct to home page
+                      print('home');
                     },
                   ),
                 ),
@@ -117,7 +118,7 @@ class _LoginState extends State<Login> {
             ),
             new InkWell(
               splashColor: Color(0xFF424242),//it revealed white only when it's in a pressed state
-              onTap: () => Navigator.of(context).pop('/home'), //Actions when tapping the button
+              onTap: () => Navigator.of(context).pushNamed('/home'), //Actions when tapping the button
               child: new Container(
                 //width: 100.0,
                 height: 35.0, //Button size
@@ -128,7 +129,6 @@ class _LoginState extends State<Login> {
                 ),
                 child: new Center(child: new Text('Log in with Google account', style: new TextStyle(fontSize: 16.0, color: Colors.orangeAccent),),),
               ),
-
             )
           ],
         ),
